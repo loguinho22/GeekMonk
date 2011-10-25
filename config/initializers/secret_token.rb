9d6bd09c0c2ddf5de9cdf6b11b7c13b3eaa7dc51
Fernando Andrade <log22@mai.l.to>
2011-10-24 22:27:20 -0200

    GeekMonkeys Development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekMonkeys::Application.config.secret_token = '99fd96c7353c1c4477f554706eb566ae3dd6f72167a376087e37ad803e50dd50eaf31b6e757a7125985a37e4ec21cc1620e4034dc6e18bd11d015e45d1154f5c'
