fe25cd0692db9a2bd9c8573ab622ad868f6e0fe7
Fernando Andrade <log22@mai.l.to>
2011-10-24 22:46:09 -0200

    Mudanças no Adicionado Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+
+   def index
+      @noticias = Noticia.find(:all)
+   end
+
+   def show
+      @noticias = Noticia.find(params[:id])
+   end   
+
 end
