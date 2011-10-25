d082f4ec54370556c13b151d5379c78401fffcb8
Fernando Andrade <log22@mai.l.to>
2011-10-24 22:40:14 -0200

    Mudanças no Route e novo controller

---
+++ db/migrate/20111025002834_create_noticia.rb
@@ -0,0 +1,16 @@
+class CreateNoticia < ActiveRecord::Migration
+  def self.up
+    create_table :noticia do |t|
+      t.string :titulo
+      t.text :texto
+      t.string :autor
+    
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :noticia
+  end
+end
